c53af05f51f4bebf34721bf233537755ca0d403a
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2020-11-13 17:35:09 -0600

    Update fast-compile.sh

--- fast-compile.sh
+++ fast-compile.sh
@@ -3,4 +3,3 @@
 "/mnt/hgfs/extern/lab/openapi-generator/mvnw" install -f "/mnt/hgfs/extern/lab/openapi-generator/modules/openapi-generator-cli/pom.xml" -Djacoco.skip=true -Dmaven.test.skip=true
 cp modules/openapi-generator-cli/target/openapi-generator-cli.jar /mnt/hgfs/extern/workbench/parental-control/languagefilter/api/
 java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar list
-
